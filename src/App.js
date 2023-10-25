a94993a7f38fc733df5a251738fc861275832fc5
GidyMane <muragegideon2000@gmail.com>
2023-10-26 01:56:10 +0300

    Add React Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
-import Article from "./Article";
-import Comment from "./Comment";
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+{/* write an <About> component here */}
+
+function About(){
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div>
-      <Article />
-      <Comment />
+      <NavBar />
+      {/* add the <Home> component here */}
+      <Home/>
+      <About/>
+      {/* add your <About> component here */}
     </div>
   );
 }
